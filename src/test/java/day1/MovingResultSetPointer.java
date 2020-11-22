4f46484201ad97de691806b828fc0d0f45a9314b
Agit AY <47agitay@gmail.com>
2020-11-22 13:14:47 -0800

    ResultSet going back up and down

---
+++ src/test/java/day1/MovingResultSetPointer.java
@@ -0,0 +1,48 @@
+package day1;
+
+import java.sql.*;
+
+public class MovingResultSetPointer {
+
+    public static void main(String[] args) throws SQLException {
+
+
+        //we want to create a statement object that generate
+        //ResulSet that can move forward and backward anytime
+
+        String connectionStr = "jdbc:oracle:thin:@52.201.187.226:1521:XE";
+        String username = "hr" ;
+        String password = "hr" ;
+
+        Connection conn = DriverManager.getConnection(connectionStr,username,password) ;
+        //we want to create a statement object that generate
+        //ResulSet that can move forward and backward anytime
+        Statement stmt = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,ResultSet.CONCUR_READ_ONLY);
+
+        ResultSet rs   =   stmt.executeQuery("SELECT * FROM REGIONS") ;
+
+        while(rs.next()){
+            System.out.println("REGION_NAME "+rs.getString("REGION_NAME"));
+        }
+
+//        rs.previous();
+//        System.out.println("REGION_NAME "+rs.getString("REGION_NAME"));
+
+        while (rs.previous()){
+            System.out.println("BACKWARD - REGION_NAME "+rs.getString("REGION_NAME"));
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
--- src/test/java/day1/TestConnection.java
+++ src/test/java/day1/TestConnection.java
@@ -9,11 +9,35 @@ public class TestConnection {
         String connectionStr = "jdbc:oracle:thin:@3.88.133.239:1521:XE";
         String username = "hr";
         String password = "hr";
+
         Connection conn = DriverManager.getConnection(connectionStr, username, password);
-        Statement stmnt = conn.createStatement();
+
+        Statement stmnt = conn.createStatement();//help us to execute query
+
         ResultSet rs = stmnt.executeQuery("SELECT * FROM REGIONS");
+//this will move the cursor from beforefirst location to first row
         rs.next(); // currently we are at the very first row
+        // rs.getString(column index) can be used to read the data of that column in current row
         System.out.println("first column value using index: --> " + rs.getString(1));
+        System.out.println("second column value using index: --> " + rs.getString(2));
+
+        //  rs.getString(column LABEL)
+        System.out.println("Region_ID at this row is : " + rs.getString("REGION_ID"));
+        System.out.println("Region_Name at this row is : " + rs.getString("REGION_NAME"));
+
+        rs.next(); //this will move us to next row so we can read next row
+        System.out.println("Region_ID at this row is : " + rs.getString("REGION_ID"));
+        System.out.println("Region_Name at this row is : " + rs.getString("REGION_NAME"));
+
+        rs.next(); //this will move us to next row so we can read next row
+        System.out.println("Region_ID at this row is : " + rs.getString("REGION_ID"));
+        System.out.println("Region_Name at this row is : " + rs.getString("REGION_NAME"));
+
+        rs.next(); //this will move us to next row so we can read next row
+        System.out.println("Region_ID at this row is : " + rs.getString("REGION_ID"));
+        System.out.println("Region_Name at this row is : " + rs.getString("REGION_NAME"));
+
+
     }
 }
 
